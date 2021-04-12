15215a97301ef665160d683ff6ad773bc3ff78b9
Ryan <ryantupo98@gmail.com>
2021-04-12 14:54:03 +0100

    12.04.2021 quiz started ajax looking nice

--- src/java/user/mainalg.java
+++ src/java/user/mainalg.java
@@ -30,6 +30,16 @@ public class mainalg implements Serializable {
 
     @Resource(name = "jdbc/loginpool")
     DataSource dataSource;
+    
+    private int likes_sour;
+    private int likes_winey;
+    private int likes_adicic;
+    private int likes_mellow;
+    private int likes_bland;
+    private int likes_sharp;
+    private int likes_harsh;
+    private int likes_pungent;
+
 
     private boolean likes_flowery;
     private boolean likes_fruity;
@@ -40,6 +50,44 @@ public class mainalg implements Serializable {
     private boolean likes_resinous;
     private boolean likes_spicy;
     private boolean likes_carbony;
+    
+    
+       
+    
+
+
+//(old man : bitter, harsh , sour | resinous , carbony , herby)
+//
+//(basic drinker : mellow , adicic , bland | nutty , caramelly , chocolatey)
+//
+//(adventurist : winey , tart , sharp | spicey , flowery , fruity)
+//
+//(old woman : mellow , bland , winey | herby , flowery , resinous)
+//    
+//    plan of action!!!!
+//    
+//    
+//    1. get all answers
+//            
+//    2. make list of tastes for each type of drinker
+//           - if integer is over 7 then will be changed to true
+//           - if 3 or below regard as false
+//           - if 4-6 regard as null
+//           
+//        make an alg that checks how many words/flavours the user has in common with certain coffee catogories
+//    
+//    3.assign the user to the table with catogories
+//            
+//            
+//    ---------------------
+//    
+//    
+//    function for showing profile feed:
+//    
+//    just show rows of the table catogories with its top 5 coffees
+//       
+//    
+
 
     public boolean isLikes_flowery() {
         return likes_flowery;
@@ -117,37 +165,68 @@ public class mainalg implements Serializable {
         String summary = typeofaroma ? "You like" + nameofaroma : "Unchecked";
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(summary));
     }
+ 
+    public int getLikes_sour() {
+        return likes_sour;
+    }
 
+    public void setLikes_sour(int likes_sour) {
+        this.likes_sour = likes_sour;
+    }
 
-//(old man : bitter, harsh , sour | resinous , carbony , herby)
-//
-//(basic drinker : mellow , adicic , bland | nutty , caramelly , chocolatey)
-//
-//(adventurist : winey , tart , sharp | spicey , flowery , fruity)
-//
-//(old woman : mellow , bland , winey | herby , flowery , resinous)
-//    
-//    plan of action!!!!
-//    
-//    
-//    1. get all answers
-//            
-//    2. make list of tastes for each type of drinker
-//           - if integer is over 7 then will be changed to true
-//           - if 3 or below regard as false
-//           - if 4-6 regard as null
-//           
-//        make an alg that checks how many words/flavours the user has in common with certain coffee catogories
-//    
-//    3.assign the user to the table with catogories
-//            
-//            
-//    ---------------------
-//    
-//    
-//    function for showing profile feed:
-//    
-//    just show rows of the table catogories with its top 5 coffees
-//       
-//    
+    public int getLikes_winey() {
+        return likes_winey;
+    }
+
+    public void setLikes_winey(int likes_winey) {
+        this.likes_winey = likes_winey;
+    }
+
+    public int getLikes_adicic() {
+        return likes_adicic;
+    }
+
+    public void setLikes_adicic(int likes_adicic) {
+        this.likes_adicic = likes_adicic;
+    }
+
+    public int getLikes_mellow() {
+        return likes_mellow;
+    }
+
+    public void setLikes_mellow(int likes_mellow) {
+        this.likes_mellow = likes_mellow;
+    }
+
+    public int getLikes_bland() {
+        return likes_bland;
+    }
+
+    public void setLikes_bland(int likes_bland) {
+        this.likes_bland = likes_bland;
+    }
+
+    public int getLikes_sharp() {
+        return likes_sharp;
+    }
+
+    public void setLikes_sharp(int likes_sharp) {
+        this.likes_sharp = likes_sharp;
+    }
+
+    public int getLikes_harsh() {
+        return likes_harsh;
+    }
+
+    public void setLikes_harsh(int likes_harsh) {
+        this.likes_harsh = likes_harsh;
+    }
+
+    public int getLikes_pungent() {
+        return likes_pungent;
+    }
+
+    public void setLikes_pungent(int likes_pungent) {
+        this.likes_pungent = likes_pungent;
+    }
 }
